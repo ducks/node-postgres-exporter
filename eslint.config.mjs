67845d273f90802a44a7a96459adab0d45251db4
Jake Goldsborough <jake.goldsborough@pm.me>
2025-06-20 14:13:30 -0700

    skip test linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,7 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  {
+    ignores: ["tests/**"]
+  }
 ]);
